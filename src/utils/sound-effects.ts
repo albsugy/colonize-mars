43c5cb310834bee1a57101bbc0f4a340a336932b
Medhat Albsugy <albsugy@gmail.com>
2022-08-07 19:01:08 +0100

    feat: add sound effects functionality

---
+++ src/utils/sound-effects.ts
@@ -0,0 +1,11 @@
+import { Howl } from 'howler'
+
+export const openSFX = new Howl({
+  src: ['/sounds-effects/open.wav'],
+  volume: 0.3
+})
+
+export const hoverSFX = new Howl({
+  src: ['/sounds-effects/hover.mp3'],
+  volume: 0.03
+})
